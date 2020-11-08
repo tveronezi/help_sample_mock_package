08676a13a4b90b23fefaf092f8d679336d9d108f
Thiago Veronezi <thiago@veronezi.org>
2020-11-08 13:11:00 -0500

    first attempt

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,37 @@
 mod service {
     use std::fmt::Display;
 
-    pub fn do_stuff<T: Display>(value: T) -> String {
-        format!("{}", value)
+    pub trait StuffDoer {
+        fn do_stuff<T: Display>(&self, value: T) -> String {
+            format!("{}", value)
+        }
     }
 }
 
 pub mod caller {
     use super::service;
 
-    pub fn call_do_stuff(value: &str) -> String {
-        service::do_stuff(value)
+    pub fn call_do_stuff<T: service::StuffDoer>(doer: T, value: &str) -> String {
+        doer.do_stuff(value)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::fmt::Display;
+
+    struct Mock {}
+    impl super::service::StuffDoer for Mock {
+        fn do_stuff<T: Display>(&self, value: T) -> String {
+            format!("{} from mock", value)
+        }
+    }
+
     #[test]
     fn it_works() {
-        assert_eq!("my stuff".to_string(), super::caller::call_do_stuff("my stuff"));
+        assert_eq!(
+            "my stuff from mock".to_string(),
+            super::caller::call_do_stuff(Mock {}, "my stuff")
+        );
     }
 }
