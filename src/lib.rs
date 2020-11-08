ad71caa6c9d7cc72c0e12f1e27cad86effb02253
Thiago Veronezi <thiago@veronezi.org>
2020-11-08 13:35:21 -0500

    more functions/methods

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,10 @@ mod service {
             let random_stuff: u8 = rng.gen();
             format!("{} {}", value, random_stuff)
         }
+
+        fn do_other_stuff<T: Display>(&self, value: T) -> String {
+            format!("alt-{}", value)
+        }
     }
 }
 
@@ -16,7 +20,12 @@ pub mod caller {
     use super::service;
 
     pub fn call_do_stuff<T: service::StuffDoer>(doer: T, value: &str) -> String {
-        doer.do_stuff(value)
+        let my_stuff = doer.do_stuff(value);
+        format!("{} [from caller]", my_stuff)
+    }
+
+    pub fn call_do_other_stuff<T: service::StuffDoer>(doer: T, value: &str) -> String {
+        format!("{} [from caller]", doer.do_other_stuff(value))
     }
 }
 
@@ -34,7 +43,11 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(
-            "my stuff from mock".to_string(),
+            "alt-my stuff [from caller]".to_string(),
+            super::caller::call_do_other_stuff(Mock {}, "my stuff")
+        );
+        assert_eq!(
+            "my stuff from mock [from caller]".to_string(),
             super::caller::call_do_stuff(Mock {}, "my stuff")
         );
     }
