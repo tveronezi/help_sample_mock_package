53ea696be660ab3370842e84e2f0d85ad248c601
Thiago Veronezi <thiago@veronezi.org>
2020-11-08 17:13:05 -0500

    using Arc

--- src/lib.rs
+++ src/lib.rs
@@ -18,13 +18,14 @@ mod service {
 
 pub mod caller {
     use super::service;
+    use std::sync::Arc;
 
-    pub fn call_do_stuff<T: service::StuffDoer>(doer: T, value: &str) -> String {
+    pub fn call_do_stuff<T: service::StuffDoer>(doer: Arc<T>, value: &str) -> String {
         let my_stuff = doer.do_stuff(value);
         format!("{} [from caller]", my_stuff)
     }
 
-    pub fn call_do_other_stuff<T: service::StuffDoer>(doer: T, value: &str) -> String {
+    pub fn call_do_other_stuff<T: service::StuffDoer>(doer: Arc<T>, value: &str) -> String {
         format!("{} [from caller]", doer.do_other_stuff(value))
     }
 }
@@ -32,6 +33,7 @@ pub mod caller {
 #[cfg(test)]
 mod tests {
     use std::fmt::Display;
+    use std::sync::Arc;
 
     struct Mock {}
     impl super::service::StuffDoer for Mock {
@@ -42,13 +44,14 @@ mod tests {
 
     #[test]
     fn it_works() {
+        let doer = Arc::new(Mock {});
         assert_eq!(
             "alt-my stuff [from caller]".to_string(),
-            super::caller::call_do_other_stuff(Mock {}, "my stuff")
+            super::caller::call_do_other_stuff(doer.clone(), "my stuff")
         );
         assert_eq!(
             "my stuff from mock [from caller]".to_string(),
-            super::caller::call_do_stuff(Mock {}, "my stuff")
+            super::caller::call_do_stuff(doer, "my stuff")
         );
     }
 }
